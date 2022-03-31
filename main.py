4c6d2b5259969888d412836ea43cc38075e4611d
unknown <nicolasgonzalez470@gmail.com>
2022-03-30 23:39:31 -0300

    Trabajamos

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import sys
 import os
+import random
+import time
+import re
 
-palabras = 'raqueta'
+
+
+
+def inicio():
+    print("\nBienvenido al juego del ahorcado\n")
+    nombre = input("\tIngresa tu nombre: ")
+    print("\tHola " + nombre + "! Suerte, Exitos!")
+    time.sleep(2)
+    print("\tEl Juego esta por empezar\nEmpieza el ahorcado")
+    time.sleep(3)
+    return nombre
+
+
+
+#####Variables
+
+palabras = 'raqueta de tenis'.upper()
+largo = len(re.sub(r"[^a-zA-Z0-9]","",palabras))
+letras = set(re.sub(r"[^a-zA-Z0-9]","",palabras))
+letras_acertadas = []
+letras_erroneas = []
+restantes = largo - len(letras_acertadas)
+nombre = inicio()
+
+while True:
+    letra = input('Inserte letra: ').upper()
+    for i in range(len(palabras)):
+		
 
 
