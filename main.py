53deda063859e1fd941cf789175347a088bea04a
unknown <nicolasgonzalez470@gmail.com>
2022-04-01 01:33:56 -0300

    faltan algunos bugs

--- main.py
+++ main.py
@@ -16,7 +16,24 @@ def inicio():
     time.sleep(3)
     return nombre
 
+def imprimir_palabra(palabra, letras_encontradas):
+    for i in range(len(palabra)):
+        if (palabra[i] in letras_encontradas) or palabra[i] == ' ':
+            print(palabra[i], end=' ')
+        else:
+            print('_', end=' ')
+    print('\n')
 
+def validar_letra(letra, letras, letras_encontradas,letras_erroneas,vidas):
+    if letra in letras:
+        if letra not in letras_acertadas:
+            letras_encontradas.append(letra)
+        else:
+            print('Esa letra ya la has ingresado')
+    else:
+        letras_erroneas.append(letra)
+        vidas-=1
+        print(vidas)
 
 #####Variables
 
@@ -26,11 +43,12 @@ letras = set(re.sub(r"[^a-zA-Z0-9]","",palabras))
 letras_acertadas = []
 letras_erroneas = []
 restantes = largo - len(letras_acertadas)
-nombre = inicio()
+vidas = 5
+#nombre = inicio()
 
-while True:
+while (vidas > 0 and (len(letras_acertadas) != len(letras))):
+    imprimir_palabra(palabras,letras_acertadas)
     letra = input('Inserte letra: ').upper()
-    for i in range(len(palabras)):
-		
-
-
+    validar_letra(letra,letras,letras_acertadas,letras_erroneas,vidas)
+    print(vidas)
+    
